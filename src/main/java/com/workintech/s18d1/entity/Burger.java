adefeca2545e3887b85654d796a1e669ca23dc75
busenurkutluca <busenurkutluca@gmail.com>
2024-10-25 00:11:56 +0300

    hafta18gün4//güncellendi

--- src/main/java/com/workintech/s18d1/entity/Burger.java
+++ src/main/java/com/workintech/s18d1/entity/Burger.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @Data
 @NoArgsConstructor
